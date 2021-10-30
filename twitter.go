172d403edea733cd4c229ffc6900f4bd183aeda4
Hiroaki ITO <mail@isudzumi.com>
2021-10-30 22:47:52 +0900

    format

--- twitter.go
+++ twitter.go
@@ -10,26 +10,25 @@ import (
 )
 
 var (
-    consumerKey  = os.Getenv("TWITTER_OAUTH_CONSUMER_KEY")
-    consumerSecret = os.Getenv("TWITTER_OAUTH_CONSUMER_SECRET")
-    accessToken = os.Getenv("TWITTER_ACCESS_TOKEN")
-    accessTokenSecret = os.Getenv("TWITTER_ACCESS_TOKEN_SECRET")
+	consumerKey       = os.Getenv("TWITTER_OAUTH_CONSUMER_KEY")
+	consumerSecret    = os.Getenv("TWITTER_OAUTH_CONSUMER_SECRET")
+	accessToken       = os.Getenv("TWITTER_ACCESS_TOKEN")
+	accessTokenSecret = os.Getenv("TWITTER_ACCESS_TOKEN_SECRET")
 )
 
-func tweet(message string) {
-    config := oauth1.NewConfig(consumerKey, consumerSecret)
-    token := oauth1.NewToken(accessToken, accessTokenSecret)
-    httpClient := config.Client(oauth1.NoContext, token)
-    client := twitter.NewClient(httpClient)
+func Tweet(message string) {
+	config := oauth1.NewConfig(consumerKey, consumerSecret)
+	token := oauth1.NewToken(accessToken, accessTokenSecret)
+	httpClient := config.Client(oauth1.NoContext, token)
+	client := twitter.NewClient(httpClient)
 
-    tweet, resp, err := client.Statuses.Update(message, nil)
+	tweet, resp, err := client.Statuses.Update(message, nil)
 
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    defer resp.Body.Close()
+	defer resp.Body.Close()
 
-    fmt.Println(tweet.Text)
+	fmt.Println(tweet.Text)
 }
-
